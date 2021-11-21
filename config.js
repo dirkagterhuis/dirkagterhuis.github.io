fbf4d2d72cf94b38b92d0b904bbcb090cc1128c7
Dirk Agterhuis <dirk.agterhuis@new10.com>
2021-11-21 15:43:55 +0100

    chore: added root config file

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    env = process.env.NODE_ENV || 'dev',
+    localSpotifyAppClientId: 'ADD_SPOTIFY_APP_CLIENT_ID_HERE',
+    localSpotifyAppClientSecret: 'ADD_SPOTIFY_APP_CLIENT_SECRET_HERE',
+}
