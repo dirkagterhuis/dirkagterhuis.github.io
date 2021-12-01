10a139fd346680f4436aa49fe4a7217252a06f00
Dirk Agterhuis <dirk.agterhuis@new10.com>
2021-12-01 22:49:44 +0100

    chore: made prettier work for .html

--- prettier.config.js
+++ prettier.config.js
@@ -2,25 +2,32 @@ module.exports = {
     tabWidth: 4,
     printWidth: 100,
     semi: false,
-    trailingComma: "es5",
+    trailingComma: 'es5',
     bracketSpacing: true,
-    parser: "typescript",
+    parser: 'typescript',
     singleQuote: true,
-    proseWrap: "never",
+    proseWrap: 'never',
     overrides: [
         {
-            files: "**/*.json",
+            files: '**/*.json',
             options: {
-                parser: "json",
+                parser: 'json',
                 tabWidth: 2,
             },
         },
         {
-            files: ["**/*.yml", "**/*.yaml"],
+            files: ['**/*.yml', '**/*.yaml'],
             options: {
-                parser: "yaml",
+                parser: 'yaml',
                 tabWidth: 2,
+            },
+        },
+        {
+            files: ['**/*.html'],
+            options: {
+                parser: 'html',
+                tabWidth: 4,
             },
         },
     ],
-};
+}
